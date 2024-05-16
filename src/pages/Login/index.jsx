79f9e492424fa5c52b55dfe9b55ad04b56eca6ca
游艺 <youyichannel@163.com>
2024-05-16 23:02:27 +0800

    feat(init): init project (#1)

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+
+const Login = () => {
+  return (
+    <div>
+      this is login page
+      <Button type="primary">test button</Button>
+    </div>
+  );
+};
+
+export default Login;
