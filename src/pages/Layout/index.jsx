8b80e135ba7bc77adfb7963c0287e3e52b5735c1
youyi <youyichannel@163.com>
2024-05-17 16:16:59 +0800

    add basic layout

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -6,36 +6,63 @@ import {
 } from "@ant-design/icons";
 import { Layout, Menu, Popconfirm } from "antd";
 import "./index.scss";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { doUserLogout, fetchUserInfo } from "@/store/user";
 
 const { Header, Sider } = Layout;
 
 const items = [
   {
     label: "首页",
-    key: "1",
+    key: "/",
     icon: <HomeOutlined />,
   },
   {
     label: "文章管理",
-    key: "2",
+    key: "/article",
     icon: <DiffOutlined />,
   },
   {
     label: "创建文章",
-    key: "3",
+    key: "/publish",
     icon: <EditOutlined />,
   },
 ];
 
 const GeekLayout = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const onMenuClick = (route) => {
+    navigate(route.key);
+  };
+  const selectedKey = location.pathname;
+  const username = useSelector((state) => state.user.userInfo.name);
+
+  useEffect(() => {
+    dispatch(fetchUserInfo());
+  }, [dispatch]);
+
+  const logout = () => {
+    dispatch(doUserLogout());
+    navigate("/login")
+  }
   return (
     <Layout>
       <Header className="header">
         <div className="logo" />
         <div className="user-info">
-          <span className="user-name">游艺丶</span>
+          <span className="user-name">{username || "游艺丶"}</span>
           <span className="user-logout">
-            <Popconfirm title="是否确认退出?" okText="退出" cancelText="取消">
+            <Popconfirm
+              title="是否确认退出?"
+              okText="退出"
+              cancelText="取消"
+              onConfirm={logout}
+            >
               <LogoutOutlined />
               退出
             </Popconfirm>
@@ -46,13 +73,15 @@ const GeekLayout = () => {
         <Sider width={200} className="site-layout-backgrond">
           <Menu
             mode="inline"
-            defaultSelectedKeys={["1"]}
+            theme="dark"
+            selectedKeys={selectedKey}
             style={{ height: "100%", borderRight: 0 }}
             items={items}
+            onClick={onMenuClick}
           />
         </Sider>
         <Layout className="layout-content" style={{ padding: 20 }}>
-          Content
+          <Outlet />
         </Layout>
       </Layout>
     </Layout>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "@/pages/Login";
 import GeekLayout from "@/pages/Layout";
 import AuthRoute from "@/components/AuthRoute";
+import Home from "@/pages/Home";
+import Article from "@/pages/Article";
+import Publish from "@/pages/Publish";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +14,20 @@ const router = createBrowserRouter([
         <GeekLayout />
       </AuthRoute>
     ),
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "article",
+        element: <Article />,
+      },
+      {
+        path: "publish",
+        element: <Publish />,
+      },
+    ],
   },
   {
     path: "/login",
