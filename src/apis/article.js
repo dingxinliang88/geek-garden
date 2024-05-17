712050e4782b770845119e16b8f56d5cc8225743
youyi <youyichannel@163.com>
2024-05-17 23:10:36 +0800

    feat(edit): support edit article

--- src/apis/article.js
+++ src/apis/article.js
@@ -14,6 +14,15 @@ export const getArticle = async (params) => {
   return res;
 };
 
+export const getArticleById = async (articleId) => {
+  const res = await http.get(`/mp/articles/${articleId}`);
+  return res;
+};
+
 export const delArticle = async (articleId) => {
   await http.delete(`/mp/articles/${articleId}`);
+};
+
+export const updateArticle = async (articleId, params) => {
+  await http.put(`/mp/articles/${articleId}?draft=false`, params);
 };
